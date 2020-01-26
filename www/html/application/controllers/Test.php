1c58db1e447cfaa2f4a64ef187f24deb14573902
Andjel Boskovic <andjel.boskovic@fishingbooker.com>
2020-01-26 17:36:49 +0100

    remove additions from test

--- www/html/application/controllers/Test.php
+++ www/html/application/controllers/Test.php
@@ -7,11 +7,5 @@ class Test extends CI_Controller
 	public function index()
 	{
 		echo "Test";
-
-		$homeRepository = HomeRepository::getInstance();
-		$firstHome = $homeRepository->findBy(['id' => 1]);
-
-		var_dump($firstHome);
-
 	}
 }
