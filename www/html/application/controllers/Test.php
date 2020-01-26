f2ddb7aa36dd06024668f7d4fcc63f56b866ce81
Andjel Boskovic <andjel.boskovic@fishingbooker.com>
2020-01-26 15:30:17 +0100

    test new repository

--- www/html/application/controllers/Test.php
+++ www/html/application/controllers/Test.php
@@ -1,11 +1,19 @@
 <?php
 
 
+use Repository\HomeRepository;
+
 class Test extends CI_Controller
 {
 	public function index()
 	{
 		echo "Test";
+		
+		$homeRepository = HomeRepository::getInstance();
+
+		$firstHome = $homeRepository->findBy(['id' => 1]);
+
+		var_dump($firstHome);
 
 	}
 }
